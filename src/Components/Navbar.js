e8ed1fe03161f8aee0d89eb1e8799a943ead6c85
Keshav Sharma <90125877+Ref-ined@users.noreply.github.com>
2021-09-06 01:43:18 +0530

    Final
    Final

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,16 +13,12 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link " aria-current="page" href="#">Home</a>
               </li>
               {/* <li className="nav-item">
-                <Link className="nav-link" to="/about">{props.aboutText}</Link>
+                <a className="nav-link" href="/about">{props.aboutText}</a>
               </li> */}
             </ul>
-            {/* <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-primary" type="submit">Search</button>
-            </form> */}
             <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`} >
             <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Dark Mode</label>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -50,21 +50,21 @@ export default function TextForm(props) {
     return (
         <>
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>   
-            <h1>{props.heading}</h1>        
+            <h1 className="mb-2">{props.heading}</h1>        
             <div className="mb-3">
-                <textarea className="form-control" id="mybox" style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} rows={8} value={text} onClick={changeText}></textarea>
+                <textarea className="form-control" id="mybox" style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} onChange={handleOnChange} rows={8} value={text} onClick={changeText}></textarea>
             </div>
-            <button className="btn btn-primary mx-1 mb-3" onClick={handleUpClick}>Convert to Upper Case</button>
-            <button className="btn btn-primary mx-1 mb-3" onClick={handleLowClick} >Convert to Lower Case</button>
-            <button className="btn btn-primary mx-1 mb-3" onClick={handleClearClick} >Clear</button>
-            <button className="btn btn-primary mx-1 mb-3" onClick={handleRmvSpace} >Remove Spaces</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Upper Case</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick} >Convert to Lower Case</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick} >Clear</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleRmvSpace} >Remove Spaces</button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your Text Summary</h2>           
-            <p> <b>{text.split(" ").length}</b> words and <b>{text.length}</b> characters</p>
-            <p> <b>{0.008 * text.split(" ").length}</b> Minutes to read</p>
+            <p> <b>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</b> words and <b>{text.length}</b> characters</p>
+            <p> <b>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length}</b> Minutes to read</p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:"Enter Something to Preview Here"}</p>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
         </>
     )
